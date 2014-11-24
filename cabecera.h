de2d6f785a085ef9357102b53b0529ace8c71981
MrRouss <rosaura53able@gmail.com>
2014-11-24 23:34:39 +0100

    Update cabecera.h

--- cabecera.h
+++ cabecera.h
@@ -1,19 +1,27 @@
+/* >>>>>>>>>>>>>>>>>>>>>>>>Aqui se desarrollaran todas la funciones que dibujen la partida y el menu
+*/
 ifndef REVERSI_H
 #define	REVERSI_H
+#ifndef CABECERA_H_
+#define CABECERA_H_
 
 #ifdef	__cplusplus
 extern "C" {
 #endif
 #define MAX 8
 #define Vacio '.'    
-void crearTablero(char tablero[MAX + 1][MAX + 1]);
-void crearTurno();
-void cambiarTurno(char * Turno);
-void imprimirTablero(char Tablero[MAX + 1][MAX + 1]);
-void imprimirCabecera();
-void comprovarMovimiento(char Tablero[MAX + 1][MAX + 1], char ficha, int posx, int posy);
-void elegirFicha(Jugador jugador1, Jugador jugador2);
 
+void imprimirTablero(char Tablero[MAX + 1][MAX + 1]);
+void imprimirContador(char  tablero[][]);
+
+void imprimirCabecera();
+void salir();
+//extras: void imprimirEstadisticas();
+int imprimirMenu();
+
+
+
+#endif /* CABECERA_H_ */
 
 #ifdef	__cplusplus
 }
