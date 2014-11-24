2f1ecce661f68ef790431cf0f8f0af71d15f8cc2
Leglait <Leglait@users.noreply.github.com>
2014-11-24 14:21:42 +0100

    Update cabecera.h

--- cabecera.h
+++ cabecera.h
@@ -1,1 +1,22 @@
-/**/
+ifndef REVERSI_H
+#define	REVERSI_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+#define MAX 8
+    
+void crearTablero(char tablero[MAX + 1][MAX + 1]);
+void crearTurno();
+void cambiarTurno(char * Turno);
+void imprimirTablero(char Tablero[MAX + 1][MAX + 1]);
+void imprimirCabecera();
+void comprovarMovimiento(char Tablero[MAX + 1][MAX + 1], char ficha, int posx, int posy);
+void elegirFicha(Jugador jugador1, Jugador jugador2);
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* REVERSI_H */
