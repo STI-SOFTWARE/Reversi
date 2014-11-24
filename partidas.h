1d454cfdcb24ed9d210ec8a1d1268ff3233481d5
MrRouss <rosaura53able@gmail.com>
2014-11-25 00:04:49 +0100

    Update partidas.h

--- partidas.h
+++ partidas.h
@@ -4,41 +4,32 @@
  */
 #ifndef PARTIDAS_H_
 #define PARTIDAS_H_
-
 #ifdef	__cplusplus
 extern "C" {
 #endif
 
-
-void crearTablero();
-void elegirTurno();
-
-char crearTurno();
+void crearTurno();
+void elegirFicha(Jugador jugador1, Jugador jugador2);
 void cambiarTurno(char turnoActual);
 Ficha crearFicha(char turno);
 
-
 void crearTablero(char tablero[MAX + 1][MAX + 1]);
-void crearTurno();
-void cambiarTurno(char * Turno);
 
-void insertarFichaEnTablero(Ficha ficha, char tablero[][]);
+void insertarFichaEnTablero(Ficha ficha, char tablero[MAX + 1][MAX + 1]);
             
-    int jugadasPosibles(char turnoActual, char tablero[][]);//booleano, se pueden hacer jugadas?
+    int comprobarJugadasPosibles(char tablero[MAX + 1][MAX + 1], char ficha)//booleano, se pueden hacer jugadas?
     //si no se pueden hacer jugadas
     void pasarTurno();//Imprimimos la ficha que pasa turno y hacemos cambiarTurno()
-    //si se puede, hacemos un dowhile comprobando lo que recibimos por teclado
-    int comprobarTeclado(char fila, int columna);//booleano, comprobamos lo que mete el usuario
-    void guardarFicha(Ficha ficha, char tablero[][]);
-    int buscarGanador(char tablero[][]);//booleano, tenemos ganador?
-        int finDePartida(char tablero[][]);//booleano, tablero lleno?
-        char contarFichas(char tablero[][]);//devolvemos el turno con +fichas
-
-int contarFichas(char turno, char tablero[][]);
-
-void comprovarMovimiento(char Tablero[MAX + 1][MAX + 1], char ficha, int posx, int posy);
-void elegirFicha(Jugador jugador1, Jugador jugador2);
-
+    //si se puede
+    int comprobarTeclado(char fila, int columna);/*booleano, comprobamos lo que mete el usuario
+                                     hacemos un dowhile comprobando lo que recibimos por teclado*/
+    int comprobarMovimiento(char tablero[MAX + 1][MAX + 1], char ficha, int posx, int posy);
+                                                            /*/Recibe un tablero, una ficha y dos numeros
+                                                            /Devuelve 0 si la jugada no es valida y 1 si lo es.*/
+    void guardarFicha(Ficha ficha, char tablero[MAX + 1][MAX + 1]);
+    int buscarGanador(char tablero[MAX + 1][MAX + 1]);//booleano, tenemos ganador?
+        int finDePartida(char tablero[MAX + 1][MAX + 1]);//booleano, tablero lleno?
+        int contarFichas(char turno, char tablero[MAX + 1][MAX + 1]);//devolvemos el turno con todas las fichas correspondientes
 
 #ifdef	__cplusplus
 }
