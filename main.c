7719ce4b672b7464d9d1b245d768465383f1e87a
MrRouss <rosaura53able@gmail.com>
2014-11-24 23:40:38 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,23 @@
 /*
- * main.c
- *
+ * main.c               >>>>>>>>>>Aqui ejecutamos el menu, desde donde nuestro jugador se movera
  *      Author: Android
  */
 
-
 #include <stdio.h>
 #include <stdlib.h>
-
 #include "cabecera.h"
 #include "partidas.h"
-
 
 typedef struct{
     char turno;
     int ejex;
     int ejey;
 }Ficha;
+typedef struct{
+    char nombre[3];
+    char ficha;
+}Jugador;
+enum {TRUE=1, FALSE=0}Boolean;
 
 void main(){
     int opcion=-1;
@@ -29,7 +30,6 @@ void main(){
         case 2:
             salir();
             break;
-        default:
-            break;
+        default:    break;
     }
 }
