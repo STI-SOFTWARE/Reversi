0abbc7e83c177ab1632b1197bcadfb2a04365d9a
Leglait <Leglait@users.noreply.github.com>
2014-11-24 14:21:13 +0100

    Update partidas.c

--- partidas.c
+++ partidas.c
@@ -1,1 +1,23 @@
-/**/
+#include <stdio.h>
+#include "reversi.h"
+typedef struct{
+    char nombre[3];
+    char ficha;
+}Jugador;
+enum {TRUE=1, FALSE=0}Boolean;
+
+void main(){
+    int jugar=-1;
+    Turno turnoActual;
+    Jugador jugador1, jugador2;
+    char tablero[MAX][MAX];
+    crearTablero(&tablero);
+    elegirFicha(&jugador1, &jugador2);
+    crearTurno(turnoActual);
+    imprimirTablero();
+    
+    jugar=comprobarJugadasPosibles(&tablero);
+    if(jugar){
+        
+    }
+}
