99cffbdeece86899713fab12f444ab5c00b065b7
MrRouss <rosaura53able@gmail.com>
2014-11-24 23:06:11 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
 /**/
+
+
+void main(){
+    int opcion=-1;
+    imprimirCabecera();
+    opcion=imprimirMenu();
+    switch(opcion){
+        case 1: 
+            inicializarPartida();
+            break;
+        case 2:
+            salir();
+            break;
+        default:
+            break;
+    }
+}
