71f7a84b3a36788b5f800221bfa9557ceffe9cf0
MrRouss <rosaura53able@gmail.com>
2014-11-25 00:45:56 +0100

    Create inicializacion.h

---
+++ inicializacion.h
@@ -0,0 +1,21 @@
+/* >>>>>>>>>>>>>>>>>>>>>>>>Aqui se desarrollaran todas la funciones que dibujen la partida y el menu      */
+#ifndef INICIALIZACION_H_
+#define INICIALIZACION_H_
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+
+
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+#endif /* INICIALIZACION_H_ */
