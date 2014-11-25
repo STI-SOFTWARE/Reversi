b43c64af67b3b4c3e9cc3a1a88d16990cec1fc36
Leglait <Leglait@users.noreply.github.com>
2014-11-25 20:28:32 +0100

    Update cabecera.c

--- cabecera.c
+++ cabecera.c
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include 'partidas.h'
-
 void imprimirTablero(char tablero[MAX+1][MAX+1]){
     int i,j;
     for(i=0; i<MAX+1; i++){
@@ -14,11 +11,11 @@ void imprimirTablero(char tablero[MAX+1][MAX+1]){
 int imprimirMenu(){
     int opcion=-1;
     do{
-        if(opcion!=-1 ){
+        if(opcion!=1 || opcion!=2){
             printf("\t>>>>>>>>>>>Debe elegir una opcion válida.\n");
         }
         printf("Elija una opcion de menu.\n\t 1. Jugar.\n\t 2. Salir.\n");
-        scanf("%d", opcion);
+        scanf("%d", &opcion);
     }while(opcion!=1 || opcion!=2);
         return opcion;
 }
@@ -31,14 +28,13 @@ void salir(){
 }
 void imprimirCabecera(){
     printf(">>>>>>>>>>>>>>Bienvenido a Reversi\n");
-    imprimirContador(NULL);
-    imprimirTablero(NULL);
+    //imprimirContador(NULL);
+   // imprimirTablero(NULL);
 }
 
-void imprimirContador(char tablero[][]){
+void imprimirContador(char tablero[MAX+1][MAX+1]){
     printf("x: %d \t\t\t\t o:%d", contarFichas("x", tablero), contarFichas("o", tablero));
 }
 void imprimirTurno(char turno){
     printf("\n >>>>>>>>>>>>Turno de: %s", turno);
-    
 }
